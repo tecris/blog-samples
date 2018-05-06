d0e962fc791a2fd5c94c1d622f9face90feec72c
Eugen Cristea <eugen@cristea.org>
2018-05-06 22:00:23 +0200

    fix

--- log4j2-kafka/docker/startKafkaTls.sh
+++ log4j2-kafka/docker/startKafkaTls.sh
@@ -2,12 +2,13 @@
 
 # run kafka in ssl mode
 CONTAINER=kafka.tls
+# CONTAINER=zookeeper
 # run kafka in non ssl mode
 # CONTAINER=kafka
 
 export KAFKA_HOST=`ip -o -4 addr list wlp3s0 | awk '{print $4}' | cut -d/ -f1`
 
 export KAFKA_PORT=9092
-export KAFKA_HOST=HOST_IP_ADDRESS
+#export KAFKA_HOST=HOST_IP_ADDRESS
 
 docker-compose up -d $CONTAINER
