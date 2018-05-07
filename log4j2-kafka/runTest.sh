38f722d64f54cf4e489f6cce69968ff9bfdd2828
Eugen Cristea <eugen@cristea.org>
2018-05-07 20:22:10 +0200

    git add runTest.sh

---
+++ log4j2-kafka/runTest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+docker run --rm \
+    --name maven-build \
+    -v "$(pwd)":/opt \
+    -v ~/.m2:/root/.m2 \
+    -v "$(pwd)"/tls_certs/client/client.keystore.jks:/etc/security/tls/client.keystore.jks:ro \
+    -v "$(pwd)"/tls_certs/client/client.truststore.jks:/etc/security/tls/client.truststore.jks:ro \
+    maven:3.5.3-alpine \
+    mvn -f /opt/pom.xml \
+    -Dkafka.topic=log4j2-kafka-topic \
+    -DstorePassword=salzburg \
+    -Dkafka.broker.url=192.168.0.10:9092 \
+    clean test
