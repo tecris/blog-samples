755c8026a4a0f4f87d3105e3e7113fe5169f241b
Eugen Cristea <eugen@cristea.org>
2018-05-08 19:50:07 +0200

    ready-to-go

--- log4j2-kafka/oneLiner.sh
+++ log4j2-kafka/oneLiner.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-set -x
-
-HOST_IP=192.168.0.10
-export KAFKA_HOST=$HOST_IP
 export KAFKA_PORT=9092
+export KAFKA_HOST=kafka.tls
 
-docker-compose -f docker/docker-compose.yaml up -d kafka.tls
-./runTest.sh
+docker-compose up -d kafka.tls 
+sleep 5
+docker-compose up run-test
+
+docker rm -f log4j2-kafka-test
+
+docker-compose down
