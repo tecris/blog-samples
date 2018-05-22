972e333db7210ae566cbd7d3f2ddc5c84f09607d
Eugen Cristea <eugen@cristea.org>
2018-05-22 20:19:05 +0200

    Update letsEncrypt.sh

--- letsencrypt-with-docker/letsEncrypt.sh
+++ letsencrypt-with-docker/letsEncrypt.sh
@@ -15,7 +15,7 @@ sudo docker run -it --rm --name certbot \
   -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
   certbot/certbot certonly --standalone -d $1 \
   --text --agree-tos \
-  --email eugen@cristea.org \
+  --email admin@example.com \
   --server https://acme-v01.api.letsencrypt.org/directory \
   --rsa-key-size 4096 --verbose --renew-by-default \
   --preferred-challenges http
